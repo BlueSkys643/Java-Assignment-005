b4e0736731f5b4db9d3585b83ec1b4bc7b3683c1
Sky's Home Computer <shunsinger643@gmail.com>
2022-10-01 20:43:21 -0700

    assignment finished

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -23,22 +23,85 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) {
+        return convertC2F(convertK2C(kelvin));
+    }
+
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
     }
 
-    public static String getUnitChoice() {
+    public static void getUnitChoice() {
         System.out.printf("%-4s : %40s%n", "C).", "To convert a °C temperature");
         System.out.printf("%-4s : %40s%n", "F).", "To convert a °F temperature");
         System.out.printf("%-4s : %40s%n", "K).", "To convert a °K temperature");
         System.out.printf("%-4s : %40s%n", "Q).", "To quit");
-        return input.nextLine();
+        //return input.nextLine();
     }
 
     public static void main(String[] args) {
         while(true) {
+            getUnitChoice();
+            System.out.println("convert from: ");
+            String from = input.nextLine();
+            if (from.equals("Q") || from.equals("q")) {
+                break;
+            }
+            System.out.println("convert to: ");
+            String to = input.nextLine();
+            if (to.equals("Q") || to.equals("q")) {
+                break;
+            }
+        //    System.out.println("Temperature to be converted from: ");
+        //    double fromValue = input.nextDouble();
+            double fromValue = getTemp(from);
+            double toValue = 0.0;
 
+            switch (from) {
+                case "K":
+                case "k":
+                    switch (to) {
+                        case "F", "f" -> {
+                            toValue = convertK2F(fromValue);
+                            System.out.println("K to F");
+                        }
+                        case "C", "c" -> {
+                            toValue = convertK2C(fromValue);
+                            System.out.println("K to C");
+                        }
+                    }
+                    break;
+
+                case "C":
+                case "c":
+                    switch (to) {
+                        case "F", "f" -> {
+                            toValue = convertC2F(fromValue);
+                            System.out.println("C to F");
+                        }
+                        case "K", "k" -> {
+                            toValue = convertC2K(fromValue);
+                            System.out.println("C to K");
+                        }
+                    }
+                    break;
+                case "F":
+                case "f":
+                    switch (to) {
+                        case "C", "c" -> {
+                            toValue = convertF2C(fromValue);
+                            System.out.println("F to C");
+                        }
+                        case "K", "k" -> {
+                            toValue = convertF2K(fromValue);
+                            System.out.println("F to K");
+                        }
+                    }
+                    break;
+
+            }
+            System.out.printf("Result %f %s is %f %s", fromValue, from, toValue, to);
         }
     }
 }
